818e8c65d4dbdb547baec4d5870f035521f7d861
GugaSantos <guga.santos.guga@gmail.com>
2022-03-22 17:43:19 -0300

    Requisito 04 ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  renderWithRouter(<FavoritePokemons />);
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste contém um heading h2 com o texto Page requested not found 😭;', () => {
+    renderWithRouter(<NotFound />);
+
+    const text = screen
+      .getByRole('heading', { level: 2, name: /page requested not found/i });
+
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const image = screen.getByRole('img', { name: /pikachu crying/i });
+    expect(image).toBeInTheDocument();
+
+    expect(image).toHaveAttribute('src', URL);
+  });
+});
