1cdcae81585d91ab9b178a62537c7082baecfa4f
GugaSantos <guga.santos.guga@gmail.com>
2022-03-22 18:02:05 -0300

    Requisito 03 ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,24 @@
+import { screen } from '@testing-library/react';
 import React from 'react';
 import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
 import renderWithRouter from './RenderWithRouter';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
-  renderWithRouter(<FavoritePokemons />);
+  it('Quando não houver favoritos, é exibido mensagem na tela', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const text = screen.getByText(/No favorite pokemon found/i);
+
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Testa se é exibido todos os cards de pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const DATALENGTH = 9;
+
+    const favorites = screen.getAllByTestId('pokemon-name');
+    expect(favorites).toHaveLength(DATALENGTH);
+  });
 });
